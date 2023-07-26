5d526d04892ede6fcb0bb025b67292fc07d29f2a
Illia Kopturov <topdevs.net@gmail.com>
2023-07-26 11:09:59 -0500

    Removed 'type' field to simplify the form

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,17 +1,14 @@
 import React, {useEffect} from "react";
 import { useFormik } from "formik";
 import {
-  Box,
   Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
   Heading,
   Input,
-  Select,
   Textarea,
   VStack,
-  requiredChakraThemeKeys,
 } from "@chakra-ui/react";
 import * as Yup from 'yup';
 import FullScreenSection from "./FullScreenSection";
@@ -97,16 +94,6 @@ const LandingSection = () => {
                     <div>{formik.errors.email}</div>
                   ) : null}
                 </FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select {...formik.getFieldProps('type')} id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
               </FormControl>
               <FormControl isInvalid={formik.errors.comment && formik.touched.comment}>
                 <FormLabel htmlFor="comment">Your message</FormLabel>
